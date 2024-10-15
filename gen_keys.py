8300b2176edfe461dadcf0eec7a308e890a4f1e8
Muxutruk2 <muxutruk2@users.noreply.github.com>
2024-10-15 18:35:08 +0200

    Modularized

--- gen_keys.py
+++ gen_keys.py
@@ -9,11 +9,12 @@ if not os.path.exists(KEYS_DIR):
     os.makedirs(KEYS_DIR)
 
 # Paths for private and public key files
-CLIENT_PRIVATE_KEY_PATH = os.path.join(KEYS_DIR, 'client_private_key.pem')
-CLIENT_PUBLIC_KEY_PATH = os.path.join(KEYS_DIR, 'client_public_key.pem')
+CLIENT_PRIVATE_KEY_PATH = 'client/keys/client_private_key.pem'
+CLIENT_PUBLIC_KEY_PATH =  'client/keys/client_public_key.pem'
 
-SERVER_PRIVATE_KEY_PATH = os.path.join(KEYS_DIR, 'server_private_key.pem')
-SERVER_PUBLIC_KEY_PATH = os.path.join(KEYS_DIR, 'server_public_key.pem')
+SERVER_PRIVATE_KEY_PATH = 'server/keys/server_private_key.pem'
+SERVER_PUBLIC_KEY_PATH =  'server/keys/server_public_key.pem'
+
 # Generate RSA key pair
 generate_key_pair(CLIENT_PRIVATE_KEY_PATH, CLIENT_PUBLIC_KEY_PATH)
 generate_key_pair(SERVER_PRIVATE_KEY_PATH, SERVER_PUBLIC_KEY_PATH)
