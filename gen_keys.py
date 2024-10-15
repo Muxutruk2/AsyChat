1c0ad1e2d3db78078fa384b4f1e19b4e1ef9ef93
Muxutruk2 <156070698+Muxutruk2@users.noreply.github.com>
2024-10-15 09:18:21 +0200

    Fixed typo

--- gen_keys.py
+++ gen_keys.py
@@ -13,7 +13,7 @@ CLIENT_PRIVATE_KEY_PATH = os.path.join(KEYS_DIR, 'client_private_key.pem')
 CLIENT_PUBLIC_KEY_PATH = os.path.join(KEYS_DIR, 'client_public_key.pem')
 
 SERVER_PRIVATE_KEY_PATH = os.path.join(KEYS_DIR, 'server_private_key.pem')
-SERVER_PUBLIC_KEY_PATH = os.path.join(KEYS_DIR, 'server:public_key.pem')
+SERVER_PUBLIC_KEY_PATH = os.path.join(KEYS_DIR, 'server_public_key.pem')
 # Generate RSA key pair
 generate_key_pair(CLIENT_PRIVATE_KEY_PATH, CLIENT_PUBLIC_KEY_PATH)
 generate_key_pair(SERVER_PRIVATE_KEY_PATH, SERVER_PUBLIC_KEY_PATH)
